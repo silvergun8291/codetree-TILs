9aad82bc7dd1f68137cd274a824a0291d118e6d2
silvergun8291 <81484874+silvergun8291@users.noreply.github.com>
2024-08-28 13:14:49 +0900

    Title: 함수를 이용한 369 게임, Time: 249ms, Memory: 30MB, Status: Passed - CodeTree

--- 240828/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240828/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,15 +1,19 @@
 def sol(a, b):
     num = list(range(a, b + 1))
-    target = [3, 6, 9]
+    target = ['3', '6', '9']
     count = 0
 
     for n in num:
         if n % 3 == 0:
             count += 1
-        elif (n // 10) in target or (n % 10) in target:
-            count += 1
-    
+        else:
+            for ch in str(n):
+                if ch in target:
+                    count += 1
+                    break
+
     return count
+
 
 a, b = list(map(int, input().split()))
 result = sol(a, b)
